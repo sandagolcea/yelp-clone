9eac87752c7a55dbc2f0248efc93f9d3b62b7a2e
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-20 15:54:03 +0000

    Adds more model tests for restaurant and reviews

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -6,4 +6,10 @@ describe Restaurant, :type => :model do
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+
+  it 'is not valid if the name already exists' do
+    Restaurant.create(name: "Starbucks")
+    restaurant = Restaurant.new(name: "Starbucks")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
