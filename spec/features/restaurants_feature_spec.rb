00c624d43eacb310635dce48bcdf2afd1b0e3b44
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-21 20:38:58 +0000

    Adds average customer review for restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -64,4 +64,13 @@ feature 'restaurants' do
     end
   end
 
+  describe '#average_rating' do
+    context 'no reviews' do
+      it 'returns "N/A" when there are no reviews' do
+        restaurant = Restaurant.create(name: 'The Ivy')
+        expect(restaurant.average_rating).to eq 'N/A'
+      end
+    end
+  end
+
 end
--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
+
+def leave_review(thoughts, rating)
+  visit '/restaurants'
+  click_link 'Review BOKA'
+  fill_in 'Thoughts', with: thoughts
+  select rating, from: 'Rating'
+  click_button 'Leave Review'
+end
+
+
 feature 'reviews' do
   before {Restaurant.create name: 'BOKA'}
 
@@ -13,4 +23,11 @@ feature 'reviews' do
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('very good')
   end
+
+  scenario 'displays an average rating for all reviews' do
+    leave_review('So so', '3')
+    leave_review('Great', '5')
+    expect(page).to have_content('Average rating: 4')
+  end
+
 end
